65810c0654bc4076b75afac55f7c03ad1331bad9
diegofceliv <fernando.dfcv@gmail.com>
2024-02-18 18:03:21 -0500

    feat: udistrital/sga_documentacion#49

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   assets: 'https://pruebasassets.portaloas.udistrital.edu.co/',
-  apiUrl: 'http://localhost:4207/',
+  apiUrl: 'http://localhost:4208/',
   PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
   SGA_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_mid/v1/',
 };
