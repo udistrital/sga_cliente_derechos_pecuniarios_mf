54c4a0ce3d4ce00088d464299618f18428948bf7
bfmunozm <swsbmm@gmail.com>
2024-06-06 08:52:23 -0500

    feat: Mejorar la carga de datos en la tabla de conceptos udistrital/sga_documentacion#271

--- src/app/derechos-pecuniarios/consultar-conceptos/consultar-conceptos.component.ts
+++ src/app/derechos-pecuniarios/consultar-conceptos/consultar-conceptos.component.ts
@@ -55,15 +55,11 @@ export class ConsultarConceptosComponent implements OnInit {
   }
 
   cargarDatos(event: any) {
+    this.mostrarTabla = false;
     this.cargando = true;
     this.vigenciaActual = event.value;
     let datosCargados = [];
     this.mostrarTabla = false;
-    console.log(
-      'THIS IS A PATH',
-      'derechos-pecuniarios/' + this.vigenciaActual
-    );
-
     this.sgaDerechoPecunarioMidService
       .get('derechos-pecuniarios/vigencias/' + this.vigenciaActual)
       .subscribe(
@@ -84,8 +80,10 @@ export class ConsultarConceptosComponent implements OnInit {
             });
 
             this.dataSource = new MatTableDataSource(datosCargados);
-            this.dataSource.paginator = this.paginator;
-            this.dataSource.sort = this.sort;
+            setTimeout(() => {
+              this.dataSource.paginator = this.paginator;
+              this.dataSource.sort = this.sort;
+            }, 100);
             this.mostrarTabla = datosCargados.length > 0;
           } else {
             this.popUpManager.showAlert(
